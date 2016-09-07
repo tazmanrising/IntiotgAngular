2cf28866c1bb00fc316d0d51777ddff170d6add1
Tom <tazmanrising@gmail.com>
2016-09-07 14:27:09 -0700

    details device status color

--- ireleases/devregmain/app/devices/deviceDetailCtrl.js
+++ ireleases/devregmain/app/devices/deviceDetailCtrl.js
@@ -4,23 +4,23 @@
     angular
         .module("deviceManagement")
         .controller("DeviceDetailCtrl",
-                    ["device",DeviceDetailCtrl]);
-        // .controller("DeviceDetailCtrl",
-        //             ["device",
-        //              DeviceDetailCtrl]);
+        ["$scope","device", DeviceDetailCtrl]);
+    // .controller("DeviceDetailCtrl",
+    //             ["device",
+    //              DeviceDetailCtrl]);
 
-    function DeviceDetailCtrl(device) {   // pass in parameter into the function , now we need
-        
+    function DeviceDetailCtrl($scope, device) {   // pass in parameter into the function , now we need
+
         //console.log("test");
 
         var vm = this;
 
         vm.device = device;
-        
+
 
         // http://localhost:42822/api/device/00022B510000000A0001
 
-        
+
         // vm.device = {
         //     "DeviceId": 1,
         //     "deviceName": "blah",
@@ -36,8 +36,39 @@
 
         vm.title = "Device Detail"; //: " + vm.device.deviceName;
 
-        if (vm.device.tags) {
-            vm.device.tagList = vm.device.tags.toString();
+        // if (vm.device.tags) {
+        //     vm.device.tagList = vm.device.tags.toString();
+        // }
+
+        $scope.set_color = function (device) {
+
+            var fontColor = "";
+            switch (device.DeviceStatus) {
+                case 0:
+                    fontColor = "gray";
+                    break;
+                case 1:
+                    fontColor = "blue";
+                    break;
+                case 2:
+                    fontColor = "green";
+                    break;
+                case 3:
+                    fontColor = "orange";
+                    break;
+
+            }
+
+            return { color: fontColor };
+
+            // if (device.DeviceStatus > 1) {
+            //     return { color: "red" }
+            // }
         }
+
+
+
+
+
     }
-}());
+} ());

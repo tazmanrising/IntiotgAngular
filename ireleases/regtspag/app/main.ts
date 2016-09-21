5fc95af4b4b956e2f8b5cfe36567e9cb1abb8f2b
Tom <tazmanrising@gmail.com>
2016-09-20 17:37:34 -0700

    adding in angular 2 paging example

---
+++ ireleases/regtspag/app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by L on 8/29/2016.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);

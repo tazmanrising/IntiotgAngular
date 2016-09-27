06e9edb667f3e8d626d7b8c9dbe7b82e713e83e0
Tom <tazmanrising@gmail.com>
2016-09-26 21:21:43 -0700

    ang 2 routes

--- ireleases/devregcut/app/app.routes.js
+++ ireleases/devregcut/app/app.routes.js
@@ -1,12 +1,10 @@
+//import { provideRouter, RouterConfig } from '@angular/router';
+//import { Routes, RouterModule } from '@angular/router';
 "use strict";
 var router_1 = require('@angular/router');
-var welcome_component_1 = require('./home/welcome.component');
-var device_routes_1 = require('./devices/device.routes');
-exports.routes = [
-    { path: '', redirectTo: '/welcome', pathMatch: 'full' },
-    { path: 'welcome', component: welcome_component_1.WelcomeComponent }
-].concat(device_routes_1.DeviceRoutes);
-exports.APP_ROUTER_PROVIDERS = [
-    router_1.provideRouter(exports.routes)
+var appRoutes = [
+    { path: '', component: WelcomeComponent } //,
 ];
+exports.appRoutingProviders = [];
+exports.routing = router_1.RouterModule.forRoot(appRoutes);
 //# sourceMappingURL=app.routes.js.map

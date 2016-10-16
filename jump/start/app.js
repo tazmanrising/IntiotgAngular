50c96ecc7f7820566040daba2c883f23d1852fdd
Tom <tazmanrising@gmail.com>
2016-10-16 15:25:26 -0700

    options commented

--- jump/start/app.js
+++ jump/start/app.js
@@ -1,3 +1,15 @@
+
+// # option 1
 //angular.module('customersApp', []);
 
-var salesApp = angular.module('salesApp', []);
+// # option 2 
+//var salesApp = angular.module('salesApp', []);
+
+
+// # 3
+
+(function () {
+    // not in global scope 
+    var salesApp = angular.module('salesApp', []);
+
+}());

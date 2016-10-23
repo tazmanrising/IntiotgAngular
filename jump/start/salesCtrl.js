9e9df144f26fef1ad04a4522800c6a9a6576f0a6
Tom Stickel <tazmanrising@gmail.com>
2016-10-23 01:18:00 -0700

    file upload poc start

--- jump/start/salesCtrl.js
+++ jump/start/salesCtrl.js
@@ -22,16 +22,31 @@
 
     console.log('controller outside fx');
     var SimpleController = function ($scope) {
-       
-       console.log('controller inside fx');
+
+        console.log('controller inside fx');
 
         $scope.customers = [
             { name: 'Dave', city: 'Phoenix' },
             { name: 'Tom', city: 'Seattle' }
         ];
 
-        
+        $scope.add = function () {
+            var f = document.getElementById('file').files[0],
+                r = new FileReader();
+            r.onloadend = function (e) {
+                var data = e.target.result;
+                console.log('send data');
+                //http://stackoverflow.com/questions/18571001/file-upload-using-angularjs
+                
+                //send your binary data via $http or $resource or do anything else with it
+            }
+            r.readAsBinaryString(f);
+        }
+
+
     };
+
+
 
     // angular.module('salesApp')
     //     .controller('SimpleController', ['$scope', function ($scope) {
@@ -43,8 +58,8 @@
 
     SimpleController.$inject = ['$scope'];
 
-        angular.module('salesApp')
-       .controller('SimpleController', SimpleController);
+    angular.module('salesApp')
+        .controller('SimpleController', SimpleController);
 
 
 } ());

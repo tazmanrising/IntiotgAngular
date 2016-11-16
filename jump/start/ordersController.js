4d87bcc4bab6579de8dc73c1a95fa1eed0299a37
Tom <tazmanrising@gmail.com>
2016-11-16 14:45:44 -0700

    ordercontroller and simbit ang1 and ang2

--- jump/start/ordersController.js
+++ jump/start/ordersController.js
@@ -12,33 +12,86 @@
 
         function init() {
             // Search the customers for the customerId
-            for(var i=0)
-        }
-
-
-        //console.log('controller inside fx');
-
-        $scope.customers = [
-            { name: 'Dave', city: 'Phoenix' },
-            { name: 'Tom', city: 'Seattle' }
-        ];
-
-        $scope.add = function () {
-            var f = document.getElementById('file').files[0],
-                r = new FileReader();
-            r.onloadend = function (e) {
-                var data = e.target.result;
-                console.log('send data');
-                //http://stackoverflow.com/questions/18571001/file-upload-using-angularjs
-                
-                //send your binary data via $http or $resource or do anything else with it
+            for(var i=0, len=$scope.customers.length;i<len;i++){
+                if($scope.customers[i].id === parseInt(customerId)){
+                    $scope.orders = $scope.customers[i].orders;
+                    break;
+                }
             }
-            r.readAsBinaryString(f);
         }
 
 
-    };
-OrdersController.$inject = ['$scope', '$routeParams'];
+               $scope.customers= [
+            {
+                id: 1, 
+                joined: '2000-12-02', 
+                name:'John', 
+                city:'Chandler', 
+                orderTotal: 9.9956,
+                orders: [
+                    {
+                        id: 1,
+                        product: 'Shoes',
+                        total: 9.9956
+                    }
+                ]
+            }, 
+            {
+                id: 2, 
+                joined: '1965-01-25',
+                name:'Zed', 
+                city:'Las Vegas', 
+                orderTotal: 19.99,
+                orders: [
+                    {
+                        id: 2,
+                        product: 'Baseball',
+                        total: 9.995
+                    },
+                    {
+                        id: 3,
+                        product: 'Bat',
+                        total: 9.995
+                    }
+                ]
+            },
+            {
+                id: 3, 
+                joined: '1944-06-15',
+                name:'Tina', 
+                city:'New York', 
+                orderTotal:44.99,
+                orders: [
+                    {
+                        id: 4,
+                        product: 'Headphones',
+                        total: 44.99
+                    }
+                ]
+            }, 
+            {
+                id: 4, 
+                joined: '1995-03-28',
+                name:'Dave', 
+                city:'Seattle', 
+                orderTotal:101.50,
+                orders: [
+                    {
+                        id: 5,
+                        product: 'Kindle',
+                        total: 101.50
+                    }
+                ]
+            }
+        ]; // end scope.customers
+        
+        init();
+    }; // end controller 
+
+
+    
+
+    OrdersController.$inject = ['$scope', '$routeParams'];
 
     angular.module('salesApp')
         .controller('OrdersController', OrdersController);

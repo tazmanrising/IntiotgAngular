c45e5b0a25279106043c2be91e5c8aa079f78103
Tom <tazmanrising@gmail.com>
2016-11-18 00:05:17 -0700

    directive add

---
+++ jump/start/directives/statusDirective.js
@@ -0,0 +1,10 @@
+(function(){
+    
+    angular.module('salesApp')
+        .directive('statusStuff', function(){
+            return{
+                template: 'Status stuff from Server'
+            };
+        })
+
+}());

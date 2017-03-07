d3eddcd82b60d709cc6bd0b250ba1b4831cdd033
tazmanrising <tazmanrising@gmail.com>
2017-03-07 00:21:10 -0700

    yeoman stuff

---
+++ Plural/yeomanstuff/angular-stuff/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularStuffApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularStuffApp
+ */
+angular.module('angularStuffApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

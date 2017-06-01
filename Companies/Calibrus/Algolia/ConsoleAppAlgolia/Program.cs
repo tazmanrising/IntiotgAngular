71184d2232c3a2a3dfafc6f9c622c2a8d3211f45
tazmanrising <tazmanrising@gmail.com>
2017-05-31 23:03:53 -0700

    algolia , node

---
+++ Companies/Calibrus/Algolia/ConsoleAppAlgolia/Program.cs
@@ -0,0 +1,24 @@
+﻿using Algolia.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAlgolia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AlgoliaClient client = new AlgoliaClient("SCVMS5FE3S", "4f9af7603070818440603e882fa48cbf");
+
+
+
+
+        }
+
+    }
+
+}
